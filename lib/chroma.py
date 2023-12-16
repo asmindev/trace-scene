d6bb4a553eb4745927cd6db85f151f0dfca7fe31
asmindev <asmin987asmin@gmail.com>
2023-12-16 14:07:44 +0800

    override sqlite

--- lib/chroma.py
+++ lib/chroma.py
@@ -1,7 +1,11 @@
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb
 
 
-client = chromadb.PersistentClient(path="tes")
+client = chromadb.PersistentClient(path=".chroma_data")
 collection = client.get_or_create_collection(
     name="vectorstore", metadata={"hnsw:space": "cosine"}
 )
